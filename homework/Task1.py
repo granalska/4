065074442f1368a1c7c617a5e91149137a7ad485
kgrnl <granalska.ss@gmail.com>
2025-10-15 00:39:29 +0300

    https://git-scm.com/docs/git-commit

--- homework/Task1.py
+++ homework/Task1.py
@@ -5,14 +5,13 @@ def get_days_from_today(input_date):
     return (date_now - input_date).days
 #вводимо дату та прописуємо помилку, далі виводимо результат
 try:
-    date_string = input("Введіть дату у форматі (дд.мм.рррр): ")
-    date_input = datetime.strptime(date_string, "%d.%m.%Y").date()
+    date_string = "2020-10-09"
+    #конвертація date
+    date_input = datetime.strptime(date_string, "%Y-%m-%d").date()
+    
     days_from_input = get_days_from_today(date_input)
-    print(f"Days from {date_string}: {days_from_input}")
+    #вивід
+    print(f"Кількість днів від {date_string}: {days_from_input}")
 
-    date_string_my_b = "01.01.2001"
-    date_my_b = datetime.strptime(date_string_my_b, "%d.%m.%Y").date()
-    days_from_my_b = get_days_from_today(date_my_b)
-    print(f"Days from my birthday: {days_from_my_b}")
 except ValueError:
-    print("Неправильний формат. Спробуйте ще раз.")
+    print("Неправильний формат дати. Використовуйте формат 'РРРР-ММ-ДД'.")

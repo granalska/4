896ceebc09d952394063b50a9d2919f1457eba61
kgrnl <granalska.ss@gmail.com>
2025-10-15 00:51:52 +0300

    https://github.com/granalska/4-.git

--- homework/Task3.py
+++ homework/Task3.py
@@ -1,21 +1,39 @@
 import re
 
-#база даних
-phone_numbers = [
-     "    +38(050)123-32-34",
-   "     0503451234",
-    "(050)8889900",
-    "38050-111-22-22",
-    "38050 111 22 11   "]
+#функ нормалізації 
+def normalize_phone_numbers():
+    #список номерів
+    phone_numbers = [
+        "+38(050)123-32-34",
+        "0503451234",
+        "(050)8889900",
+        "38050-111-22-22",
+        "38050 111 22 11"
+    ]
 
-#прибираємо зайве
-cleaned = [
-    "+38" + r if r.startswith("0") else "+" + r
-    for r in (re.sub(r"[^\d]", "", p) for p in phone_numbers)
-]
-unique = dict.fromkeys(cleaned)
+    unique_numbers = set()
+
+    for phone in phone_numbers:
+        #залишаємо лише цифри 
+        cleaned = re.sub(r'\D', '', phone)
+
+        #додаємо код країни
+        if cleaned.startswith('0'):
+            normalized = '+38' + cleaned
+        elif cleaned.startswith('380'):
+            normalized = '+' + cleaned
+        else:
+            
+            continue
+
+        # Перевірка довжиги номера
+        if len(normalized) == 13:
+            unique_numbers.add(normalized)
+
+    return unique_numbers
+
 
 #виведення
-for n in unique:
-    if re.match(r"^\+380\d{9}$", n):
-       print(n)
+print("Нормалізовані номери телефонів:")
+for number in normalize_phone_numbers():
+    print(number)

491352df081fb3102af654bb325b54bc0264cc68
kgrnl <granalska.ss@gmail.com>
2025-10-22 21:59:48 +0300

    yu

--- 6/Task4..py
+++ 6/Task4..py
@@ -1,7 +1,36 @@
-contacts = {}  #контакти
+def add_contact(args,contacts):
+    if len(args) < 2:
+        return("Використання: add Ім'я yомер")
+     
+        name, phone = args
+        contacts[name] = phone
+        return f"Контакт {name} додано."    
+    
+    def change_contact(args,contacts):
+        if len(args) < 2:
+            return("Використання: change Ім'я новий_номер")
+        name, new_phone = args
+        if name in contacts:
+            contacts[name] = new_phone
+            return f"Номер для {name} змінено."
+        else:
+            return "контакт не здайдено у базі."
+        
+    def show_all_contacts(contacts):
+            if contacts:
+                result = []
+                for name, phone in contacts.items():
+                    result.append(f"{name}: {phone}")
+                return "\n".join(result)
+            else:
+                return "Список контактів порожній."
+            
+def main():     
 
-print("Привіт! Я твій бот-помічник🤖")
-print("Команди: add, change, phone, all, exit")
+    contacts = {}  #контакти
+
+    print("Привіт! Я твій бот-помічник🤖")
+    print("Команди: add, change, phone, all, exit")
 
 while True:
     user_input = input(">>> ").strip().split()
@@ -55,3 +84,7 @@ while True:
 
     else:
         print("Невідома команда. Перевірте правильність написання.")
+
+if __name__ == "__main__":
+    main()  
+    

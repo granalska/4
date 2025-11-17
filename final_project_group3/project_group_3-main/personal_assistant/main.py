c451996dabd55b5fdb81b05f0c0c15db01a589ea
kgrnl <granalska.ss@gmail.com>
2025-11-17 22:25:05 +0200

    new

--- final_project_group3/project_group_3-main/personal_assistant/main.py
+++ final_project_group3/project_group_3-main/personal_assistant/main.py
@@ -23,7 +23,7 @@ def input_error(func):
         try:
             return func(*args, **kwargs)
         except ValueError as e:
-            return str(e)
+            return str(e) 
         except KeyError:
             return "Contact not found."
         except IndexError:
